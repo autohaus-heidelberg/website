6c7f1fabdd471dfa567690416f0e4277413cdf53
Leo Reich <reich.leo@gmail.com>
2024-05-12 11:09:50 +0200

    fix: deploy script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -10,7 +10,7 @@ import  fs from "fs";
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     // Copy the page to 404.html, this way accessing routes directly will work
-    await execa("cp", [folderName + "/index.html"], [folderName + "/404.html"])
+    await execa("cp", [folderName + "/index.html", folderName + "/404.html"])
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
