8ffdf2ab64d7c22a974e105d368194222b263285
root <root@ct-leo.fritz.box>
2025-01-23 18:04:04 +0000

    chore: add event test9

--- src/events.ts
+++ src/events.ts
@@ -499,4 +499,24 @@ Flyer: <a href="https://www.instagram.com/joey.controlletti/">@joey.controlletti
     }
   ]
 }
+,
+  {
+  "id": "test9",
+  "date": "2025-01-03T20:00",
+  "title": "test9",
+  "descriptionShort": "test",
+  "descriptionLong": "",
+  "fee": "10",
+  "artists": [
+    {
+      "name": "yesy",
+      "link": "",
+      "description": "",
+      "soundcloud": "",
+      "youtube": "",
+      "bandcamp": ""
+    }
+  ],
+  "shopLink": "https://pretix.eu/carousel/test9/"
+}
 ]
