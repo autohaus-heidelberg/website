8016a73b66edcb714eed795e17083ccb477a315d
root <root@ct-leo.fritz.box>
2025-01-23 17:41:45 +0000

    chore: add event test1245766

--- src/events.ts
+++ src/events.ts
@@ -480,4 +480,23 @@ Flyer: <a href="https://www.instagram.com/joey.controlletti/">@joey.controlletti
       }
     ],
   }
+,
+  {
+  "id": "test1245766",
+  "date": "2025-01-03T20:00",
+  "title": "test6",
+  "descriptionShort": "test",
+  "descriptionLong": "",
+  "fee": "10",
+  "artists": [
+    {
+      "name": "yesy",
+      "link": "",
+      "description": "",
+      "soundcloud": "",
+      "youtube": "",
+      "bandcamp": ""
+    }
+  ]
+}
 ]
