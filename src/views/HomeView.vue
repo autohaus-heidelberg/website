0964f0ee9bf1d432af3c2edcdb49e868936e0dc1
Leo Reich <reich.leo@gmail.com>
2025-06-15 11:09:27 +0200

    fix: broken events, layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -69,92 +69,93 @@ p Wir sind das Carousel im alten Autohaus.
     .event.mb-1(v-else, v-for="event in upcoming", :key="event.id")
       EventPreview(:event="event")
 
-  .newsletter
-    h1 Newsletter
-    p Abonniere den Newsletter um updates über anstehende Konzerte und Veranstaltungen zu erhalten. Wir senden dir meistens einen Newsletter im Monat über die anstehenden Veranstaltungen und eine Erinnerung am Tag der Veranstaltung.
-    .newsletter-form
-      #mc_embed_shell
-        //- link(href="//cdn-images.mailchimp.com/embedcode/classic-061523.css", rel="stylesheet", type="text/css")
-        #mc_embed_signup
-          form#mc-embedded-subscribe-form.validate(
-            action="https://gmail.us17.list-manage.com/subscribe/post?u=96eb1b394c29f8ffd1ab89b4a&amp;id=83bbee113a&amp;f_id=00f8c2e1f0",
-            method="post",
-            name="mc-embedded-subscribe-form",
-            target="_blank"
-          )
-            #mc_embed_signup_scroll
-              .mc-field-group
-                label(for="mce-EMAIL") E-Mail-Adresse
-                  |
-                  span.asterisk *
-                input#mce-EMAIL.required.email(
-                  type="email",
-                  name="EMAIL",
-                  required,
-                  value
+  .content
+    .newsletter
+      h1 Newsletter
+      p Abonniere den Newsletter um updates über anstehende Konzerte und Veranstaltungen zu erhalten. Wir senden dir meistens einen Newsletter im Monat über die anstehenden Veranstaltungen und eine Erinnerung am Tag der Veranstaltung.
+      .newsletter-form
+        #mc_embed_shell
+          //- link(href="//cdn-images.mailchimp.com/embedcode/classic-061523.css", rel="stylesheet", type="text/css")
+          #mc_embed_signup
+            form#mc-embedded-subscribe-form.validate(
+              action="https://gmail.us17.list-manage.com/subscribe/post?u=96eb1b394c29f8ffd1ab89b4a&amp;id=83bbee113a&amp;f_id=00f8c2e1f0",
+              method="post",
+              name="mc-embedded-subscribe-form",
+              target="_blank"
+            )
+              #mc_embed_signup_scroll
+                .mc-field-group
+                  label(for="mce-EMAIL") E-Mail-Adresse
+                    |
+                    span.asterisk *
+                  input#mce-EMAIL.required.email(
+                    type="email",
+                    name="EMAIL",
+                    required,
+                    value
+                  )
+                #mce-responses.clear.foot
+                  #mce-error-response.response(style="display: none")
+                  #mce-success-response.response(style="display: none")
+                div(
+                  style="position: absolute; left: -5000px",
+                  aria-hidden="true"
                 )
-              #mce-responses.clear.foot
-                #mce-error-response.response(style="display: none")
-                #mce-success-response.response(style="display: none")
-              div(
-                style="position: absolute; left: -5000px",
-                aria-hidden="true"
-              )
-                | /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
-                input(
-                  type="text",
-                  name="b_96eb1b394c29f8ffd1ab89b4a_83bbee113a",
-                  tabindex="-1",
-                  value
-                )
-              button#mc-embedded-subscribe.mt-1.button.el-button.el-button--default(
-                type="submit",
-                value="Anmelden",
-                name="subscribe"
-              ) Anmelden
+                  | /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
+                  input(
+                    type="text",
+                    name="b_96eb1b394c29f8ffd1ab89b4a_83bbee113a",
+                    tabindex="-1",
+                    value
+                  )
+                button#mc-embedded-subscribe.mt-1.button.el-button.el-button--default(
+                  type="submit",
+                  value="Anmelden",
+                  name="subscribe"
+                ) Anmelden
 
-  .directions
-    h1 Lage
-    p Das Carousel im alten Autohaus befindet sich in der
-      |
-      |
-      a(href="https://maps.app.goo.gl/Dp9BHeBE5aU8KwQH6") Hebelstraße 7
-      |
-      | am einfachsten mit dem Fahrrad oder mit der Bahn erreichbar, die nächste Haltestelle ist die 
-      a(href="https://maps.app.goo.gl/7k4UvxVHtGvHK1NfA") Rudolf-Diesel-Straße
-    a.mb-1(href="https://maps.app.goo.gl/Dp9BHeBE5aU8KwQH6") Auf Google Maps öffnen
-    .map
-      l-map(
-        ref="map",
-        v-model:zoom="zoom",
-        :center="[49.396956872123646, 8.680584425099438]"
-      )
-        l-tile-layer(
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-          layer-type="base",
-          name="OpenStreetMap"
+    .directions
+      h1 Lage
+      p Das Carousel im alten Autohaus befindet sich in der
+        |
+        |
+        a(href="https://maps.app.goo.gl/Dp9BHeBE5aU8KwQH6") Hebelstraße 7
+        |
+        | am einfachsten mit dem Fahrrad oder mit der Bahn erreichbar, die nächste Haltestelle ist die 
+        a(href="https://maps.app.goo.gl/7k4UvxVHtGvHK1NfA") Rudolf-Diesel-Straße
+      a.mb-1(href="https://maps.app.goo.gl/Dp9BHeBE5aU8KwQH6") Auf Google Maps öffnen
+      .map
+        l-map(
+          ref="map",
+          v-model:zoom="zoom",
+          :center="[49.396956872123646, 8.680584425099438]"
         )
-        l-marker(:latLng="[49.39666, 8.680584425099438]")
+          l-tile-layer(
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            layer-type="base",
+            name="OpenStreetMap"
+          )
+          l-marker(:latLng="[49.39666, 8.680584425099438]")
 
-  .past-events
-    h1 Vergangene Events
-    p Ihr wollt wissen wie die Band gestern oder letzte Woche hieß?
-    router-link(:to="{ name: 'pastEvents' }") Hier findet ihr die vergangengen Events
+    .past-events
+      h1 Vergangene Events
+      p Ihr wollt wissen wie die Band gestern oder letzte Woche hieß?
+      router-link(:to="{ name: 'pastEvents' }") Hier findet ihr die vergangengen Events
 
-  #about
-    h1 Über uns
-    p Unser Ziel ist es niederschwellige Angebote zu schaffen für die lokale Kunst- und Kulturszene.
-    p Zum einen bieten wir Kunstschaffenden günstige Proberäume und einen Veranstaltungsort
-    p Zum anderen bieten wir Kunstinteressierten günstige Konzerte und Events
+    #about
+      h1 Über uns
+      p Unser Ziel ist es niederschwellige Angebote zu schaffen für die lokale Kunst- und Kulturszene.
+      p Zum einen bieten wir Kunstschaffenden günstige Proberäume und einen Veranstaltungsort
+      p Zum anderen bieten wir Kunstinteressierten günstige Konzerte und Events
 
-  #impressum
-    h1 Impressum
-    p Verantwortlich für den Inhalt dieser Website:
-    p Carousel e.V. - Hebelstraße 7 - 69115 Heidelberg
-    a(href="mailto:carouselev@gmail.com") carouselev@gmail.com
-    br
-    br
-    router-link(:to="{ name: 'about' }") Mehr
+    #impressum
+      h1 Impressum
+      p Verantwortlich für den Inhalt dieser Website:
+      p Carousel e.V. - Hebelstraße 7 - 69115 Heidelberg
+      a(href="mailto:carouselev@gmail.com") carouselev@gmail.com
+      br
+      br
+      router-link(:to="{ name: 'about' }") Mehr
 </template>
 
 <style scoped>
@@ -260,11 +261,7 @@ p Wir sind das Carousel im alten Autohaus.
     grid-gap: 1rem;
     column-gap: 3rem;
     grid-template-areas:
-      "dates newsletter"
-      "dates past-events"
-      "dates directions"
-      "dates about"
-      "dates impressum";
+      "dates content"
   }
 
   .dates {
